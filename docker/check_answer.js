896dc52c33dac5623ac1f2225f8b0ad6bf890839
JoshVanL <jv15626@my.bristol.ac.uk>
2018-02-05 20:21:16 +0000

    Moves commands to functions

--- docker/check_answer.js
+++ docker/check_answer.js
@@ -3,24 +3,41 @@ var Promise = require('bluebird');
 
 const getAsync = Promise.promisify(cmd.get, { multiArgs: true, context: cmd })
 
-getAsync('docker build -t mynode .').then(data => {
-    getAsync('docker run mynode').then(data => {
-        getAsync('docker cp $(docker ps -l -q):/output .').then(data => {
-            getAsync('cmp --silent answer1 output || echo "files are different"').then(data => {
-                if (data[0] != "") {
-                    console.log("files are different!");
-                } else {
-                    console.log("files are the same!");
-                }
-            }).catch(err => {
-                console.log('error:', err)
-            })
-        }).catch(err => {
-            console.log(err)
-        })
+function dockerBuild() {
+    getAsync('docker build -t mynode .').then(data => {
+        dockerRun();
     }).catch(err => {
         console.log(err)
     })
-}).catch(err => {
-    console.log(err)
-})
+}
+
+function dockerRun() {
+    getAsync('docker run mynode').then(data => {
+        dockerCp();
+    }).catch(err => {
+        console.log(err)
+    });
+}
+
+function dockerCp() {
+    getAsync('docker cp $(docker ps -l -q):/output .').then(data => {
+        compareFiles();
+    }).catch(err => {
+        console.log(err)
+
+    });
+}
+
+function compareFiles() {
+    getAsync('cmp --silent answer1 output || echo "files are different"').then(data => {
+        if (data[0] != "") {
+            console.log("files are different!");
+        } else {
+            console.log("files are the same!");
+        }
+    }).catch(err => {
+        console.log('error:', err)
+    });
+}
+
+dockerBuild();
