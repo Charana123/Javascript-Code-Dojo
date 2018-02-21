562661362ed6729426e516138c2abab99669e9f4
Charana <charananandasena@yahoo.com>
2018-02-21 01:30:27 +0000

    login works, starting a session

---
+++ site/public/js/http_requests.js
@@ -0,0 +1,27 @@
+
+var httpGetAsync = function(theUrl){
+    return new Promise((resolve, reject) => {
+        var xmlHttp = new XMLHttpRequest();
+        xmlHttp.onreadystatechange = function() { 
+            if (xmlHttp.readyState == XMLHttpRequest.DONE && xmlHttp.status == 200){
+                    resolve(xmlHttp.responseText)
+            }
+        }
+        xmlHttp.open("GET", theUrl, true); 
+        xmlHttp.send(null);
+    })
+}
+
+var httpPostAsync = function(theUrl, data){
+    return new Promise((resolve, reject) => {
+        var xmlHttp = new XMLHttpRequest();
+
+        xmlHttp.onreadystatechange = function() { 
+            if (xmlHttp.readyState == XMLHttpRequest.DONE && xmlHttp.status == 200){
+                    resolve(xmlHttp.responseText)
+            }
+        }
+        xmlHttp.open("POST", theUrl, true); 
+        xmlHttp.send(data);
+    })
+} 
