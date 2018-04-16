dfe01ecd169d0c37940e235bc0e3fcc0f1a4ca9e
JoshVanL <jv15626@my.bristol.ac.uk>
2018-04-16 17:55:28 +0100

    Adds some context to the challenge page

--- site/docker/task.js
+++ site/docker/task.js
@@ -1,3 +1,11 @@
-var a = "Hello";
-var b = "World!";
-console.log(a + " " + b);
+var num = 99
+var a = 1, b = 0, temp;
+
+console.log(0);
+while (num > 0){
+    temp = a;
+    a = a + b;
+    b = temp;
+    console.log(b);
+    num--;
+}
