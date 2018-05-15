28a2b87689ad744e49780d306b5ff732a031637e
Charana <charananandasena@yahoo.com>
2018-05-15 10:19:21 +0100

    login options and user options done

--- site/public/js/user-options.js
+++ site/public/js/user-options.js
@@ -8,6 +8,8 @@ window.addEventListener("load", function(){
     transparent_black_background.addEventListener("click", function(){
         var user_options_popup = document.getElementById("user-options-popup");
         user_options_popup.style.display = "none";
+        var login_popup = document.getElementById("login-form-popup");
+        login_popup.style.display = "none";
     })
     var login_button = document.getElementById("login-button");
     login_button.onclick = function(){
