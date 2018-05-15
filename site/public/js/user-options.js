31f59a8afa330def02ebacc8a348ec06e4cfdb2b
Charana <charananandasena@yahoo.com>
2018-05-15 02:05:49 +0100

    css animation done and javascript configured

--- site/public/js/user-options.js
+++ site/public/js/user-options.js
@@ -1,12 +1,17 @@
 window.addEventListener("load", function(){
     var profile_icon_button = document.getElementById("profile-image");
     profile_icon_button.onclick = function(){
-        var login_popup = document.getElementById("user-options-popup");
-        login_popup.style.display = "block";
+        var user_options_popup = document.getElementById("user-options-popup");
+        user_options_popup.style.display = "block";
     }
     var transparent_black_background = document.getElementById("transparent-black-background");
     transparent_black_background.addEventListener("click", function(){
-        var login_popup = document.getElementById("user-options-popup");
-        login_popup.style.display = "none";
+        var user_options_popup = document.getElementById("user-options-popup");
+        user_options_popup.style.display = "none";
     })
+    var login_button = document.getElementById("login-button");
+    login_button.onclick = function(){
+        var login_popup = document.getElementById("login-form-popup");
+        login_popup.style.display = "block";
+    }
 });
