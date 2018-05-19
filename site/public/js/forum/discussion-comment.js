53545a012a67ae2dae003711139f4c1882d78853
Charana <charananandasena@yahoo.com>
2018-05-19 14:49:03 +0100

    toggle replies

--- site/public/js/forum/discussion-comment.js
+++ site/public/js/forum/discussion-comment.js
@@ -4,3 +4,16 @@ function postReply(){
     //make an AJAX request for a 
     // reply_comment_container.innerHTML = 
 }
+
+function toggleReplies(){
+    var toggle_icon = document.getElementById("discussion-comment-toggle");
+    var reply_container = document.getElementById("reply-comment-container");
+    if(reply_container.style.display === "none"){
+        reply_container.style.display = "block";
+        toggle_icon.src = "/img/minus.svg";
+    }
+    else {
+        reply_container.style.display = "none";
+        toggle_icon.src = "/img/plus.svg";
+    }
+}
