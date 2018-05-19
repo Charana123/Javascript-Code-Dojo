1e1e68bf71a1ebfc5d0ee0852c2ffd3a8aeddab1
Charana <charananandasena@yahoo.com>
2018-05-19 14:26:01 +0100

    discussion 2 level comments ui done

---
+++ site/public/js/forum/discussion-comment.js
@@ -0,0 +1,6 @@
+function postReply(){
+    console.log("posting reply...");
+    //var reply_comment_container = document.getElementById("reply-comment-container");
+    //make an AJAX request for a 
+    // reply_comment_container.innerHTML = 
+}
