6405d403b53a3e5ace2978cb9eb0ad6deef4599e
Charana <charananandasena@yahoo.com>
2018-05-20 12:32:34 +0100

    forum_post page done

--- site/public/js/forum/discussion-comment.js
+++ site/public/js/forum/discussion-comment.js
@@ -5,9 +5,9 @@ function postReply(){
     // reply_comment_container.innerHTML = 
 }
 
-function toggleReplies(){
-    var toggle_icon = document.getElementById("discussion-comment-toggle");
-    var reply_container = document.getElementById("reply-comment-container");
+function toggleReplies(event, toggle_icon){
+    var reply_container_id = toggle_icon.getAttribute("replyContainerID");
+    var reply_container = document.getElementById(reply_container_id);
     if(reply_container.style.display === "none"){
         reply_container.style.display = "block";
         toggle_icon.src = "/img/minus.svg";
